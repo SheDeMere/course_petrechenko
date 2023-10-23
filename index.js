69af8df49c239958c244523cd85c108a036b4909
Apti <apti2500@mail.ru>
2023-10-23 15:52:39 +0300

    change lesson 1

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-}
+};
 
-const askQuestionForUser = String(prompt('Один из последних просмотренных фильмов?'));
-const rateForFilms = +prompt('На сколько оцените его?');
+const a = String(prompt('Один из последних просмотренных фильмов?')),
+      b = +prompt('На сколько оцените его?'),
+      c = String(prompt('Один из последних просмотренных фильмов?')),
+      d = +prompt('На сколько оцените его?');
 
-personalMovieDB.movies = {
-   [askQuestionForUser]: rateForFilms
-}
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB)
