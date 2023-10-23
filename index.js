fb91d1cce6d02702698d45fbd7ef0e45d876ff13
Apti <apti2500@mail.ru>
2023-10-23 17:43:40 +0300

    lesson 2

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-'use strict';
+// 'use strict';
 
-const numberOfFilms = +prompt('Сколько вы уже фильмов посмотрели?');
+// const numberOfFilms = +prompt('Сколько вы уже фильмов посмотрели?');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = String(prompt('Один из последних просмотренных фильмов?')),
-      b = +prompt('На сколько оцените его?'),
-      c = String(prompt('Один из последних просмотренных фильмов?')),
-      d = +prompt('На сколько оцените его?');
+// const a = String(prompt('Один из последних просмотренных фильмов?')),
+//       b = +prompt('На сколько оцените его?'),
+//       c = String(prompt('Один из последних просмотренных фильмов?')),
+//       d = +prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB)
+// console.log(personalMovieDB)
+
+
+
+// console.log( NaN || 2 || undefined ); //2
+
+
+// console.log( NaN && 2 && undefined ); //NaN
+
+
+// console.log( 1 && 2 && 3 ); // 3
+
+
+// console.log( !1 && 2 || !3 ); // !3
+
+
+// console.log( 25 || null && !3 ); //25
+
+
+// console.log( NaN || null || !3 || undefined || 5); // 5
+
+
+// console.log( NaN || null && !3 && undefined || 5); // 5
+
+
+// console.log( 5 === 5 && 3 > 2 || 5); //5
